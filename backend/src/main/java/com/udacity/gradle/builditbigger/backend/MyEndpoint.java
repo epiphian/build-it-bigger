47bcdcc7e3dfba91da1134b739cfca9c1ae013b1
Y <slapo@mail.ru>
2018-06-07 02:05:06 +0500

    Add Jokes library to endpoint and create endpoint's getJoke method.

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -4,6 +4,8 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
+import com.udacity.projects.gradle.jokes.MyJokes;
+
 import javax.inject.Named;
 
 /** An endpoint class we are exposing */
@@ -23,6 +25,17 @@ public class MyEndpoint {
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
         response.setData("Hi, " + name);
+
+        return response;
+    }
+
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke(){
+        MyBean response = new MyBean();
+
+        MyJokes jokes = new MyJokes();
+        String joke = jokes.getJoke();
+        response.setData("Ha, " + joke);
 
         return response;
     }
