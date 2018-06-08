eb6a250b00ac981d20592c3ea208f2dcafb5e080
Y <slapo@mail.ru>
2018-06-08 22:08:29 +0500

    Add AsyncTask for test endpoint request.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,7 +1,9 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -47,9 +49,11 @@ public class MainActivity extends AppCompatActivity {
         MyJokes jokes = new MyJokes();
         String joke = jokes.getJoke();
 
-        Intent jokeIntent = new Intent(this,JokeActivity.class);
-        jokeIntent.putExtra(Intent.EXTRA_TEXT, joke);
-        startActivity(jokeIntent);
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+
+//        Intent jokeIntent = new Intent(this,JokeActivity.class);
+//        jokeIntent.putExtra(Intent.EXTRA_TEXT, joke);
+//        startActivity(jokeIntent);
     }
 
 
